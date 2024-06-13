553bc23e0169f3e42bfeea2b0677755fd3feaf94
KateSawada <nuksawada@gmail.com>
2024-06-13 17:08:46 +0900

    formatter

--- app.py
+++ app.py
@@ -1,42 +1,45 @@
-from cgitb import text
 import csv
-from logging import root, warning
+import datetime
 import os
-from io import BytesIO
-from re import S
-# from shutil import rmtree
-import matplotlib.pyplot as plt
-from copy import deepcopy
-from copy import copy
-from sys import exit
+import platform
+import subprocess
+import tkinter as tk
 from argparse import ArgumentParser
+from cgitb import text
+from copy import copy, deepcopy
+from io import BytesIO
+from logging import root, warning
+from re import S
+from sys import exit
+from tkinter import filedialog, ttk
 from typing import Any
 
-import datetime
-
-import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import flet as ft
-from flet import ControlEvent
-
-# from PIL import Image, ImageTk
+import matplotlib.pyplot as plt
 import numpy as np
-from scipy.signal import hamming, detrend, morlet2, cwt, spectrogram, get_window, butter, sosfilt
+import pandas as pd
+import yaml
+from flet import ControlEvent
+from matplotlib import backend_tools as cbook
 from matplotlib import use
-use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
 from matplotlib.mlab import cohere, window_hanning
 from matplotlib.pyplot import specgram as pltspectrogram
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib import backend_tools as cbook
-# import PySimpleGUI as sg
-import pandas as pd
+from scipy.signal import (
+    butter,
+    cwt,
+    detrend,
+    get_window,
+    hamming,
+    morlet2,
+    sosfilt,
+    spectrogram,
+)
 from sklearn.decomposition import PCA
-import subprocess
-import platform
-import yaml
+
+use("TkAgg")
+
 
 class MainApp:
     def __init__(self) -> None:
@@ -44,7 +47,7 @@ class MainApp:
             SpectrogramAnalysis(),  # ここで解析手法のクラスをインスタンス化
             # 他の解析手法もここに追加
         ]
-        self.target_dir = ft.Text(value = "Not Selected")
+        self.target_dir = ft.Text(value="Not Selected")
         self.log_content = ft.Text()
 
     def run(self):
@@ -67,7 +70,6 @@ class MainApp:
         """
         self.run()
 
-
     def setting_field(self):
         for setting in self.analysis_methods:
             setting.build_result_ui
@@ -78,12 +80,11 @@ class MainApp:
         print(self.config)
 
     def write_config_file(self, e: ControlEvent):
-        self.config ["key1"] = "change"
+        self.config["key1"] = "change"
         self.config["add_key"] = "add"
-        with open("config.yaml", "w") as file :
+        with open("config.yaml", "w") as file:
             yaml.dump(self.config, file)
         print(self.config)
-
 
     # select folder
     def on_folder_picked(self, e: ft.FilePickerResultEvent):
@@ -108,10 +109,16 @@ class MainApp:
             def recursive_search(directory, depth):
                 if depth > 3:
                     return
-                csv_files = [f for f in os.listdir(directory) if f.endswith('.csv') and not f.endswith('.tremor.csv')]
+                csv_files = [
+                    f
+                    for f in os.listdir(directory)
+                    if f.endswith(".csv") and not f.endswith(".tremor.csv")
+                ]
                 self.file_num += len(csv_files)
                 if len(csv_files) == 2:
-                    file_list.append(tuple(os.path.join(directory, file) for file in csv_files))
+                    file_list.append(
+                        tuple(os.path.join(directory, file) for file in csv_files)
+                    )
                     self.pairs_num += 1
 
                 elif len(csv_files) == 1:
@@ -125,10 +132,10 @@ class MainApp:
             recursive_search(self.target_dir.value, 0)
 
             # log_outputsの中身更新
-            self.log_content.value  = f"{self.file_num}files\n{self.pairs_num}pairs\nanalysis files\n{file_list}"
+            self.log_content.value = f"{self.file_num}files\n{self.pairs_num}pairs\nanalysis files\n{file_list}"
 
-        else: # Not Selected なら
-            self.log_content.value  = "No folder is selected"
+        else:  # Not Selected なら
+            self.log_content.value = "No folder is selected"
 
         self.page.update()
 
@@ -137,49 +144,104 @@ class MainApp:
 
     # open result directory
     def open_result(self):
-        if platform.system() == "Windows": # Windows
-            subprocess.Popen(["explorer", self.target_dir.value], shell = True)
-        elif platform.system() == "Darwin": # Mac
+        if platform.system() == "Windows":  # Windows
+            subprocess.Popen(["explorer", self.target_dir.value], shell=True)
+        elif platform.system() == "Darwin":  # Mac
             subprocess.Popen(["open", self.target_dir.value])
-        else: # Linux
+        else:  # Linux
             subprocess.Popen(["xdg-open", self.target_dir.value])
         return
 
     def build_ui(self):
-        self.folder_picker = ft.FilePicker(on_result = self.on_folder_picked)
+        self.folder_picker = ft.FilePicker(on_result=self.on_folder_picked)
         self.page.overlay.append(self.folder_picker)
-        select_folder_button = ft.Row([ft.OutlinedButton(text = "Select Folder", on_click= self.show_pick_folder), self.target_dir])
-        scan_button = ft.OutlinedButton(text = "Scan", on_click= self.on_scan_click)
-        run_button = ft.OutlinedButton(text = "Run", on_click=self.on_run_click)
-        open_result_button = ft.OutlinedButton(text = "Open Result", on_click=self.on_open_result_click)
-        apply_button = ft.OutlinedButton(text = "Apply&Save Settings", on_click="")
-        settings = ft.Container(content = ft.Column([
-            ft.Row([ft.Text("Row start"), ft.TextField(height = 40,width=50)]),
-            ft.Row([ft.Text("Column start"), ft.TextField(height = 40,width=50)]),
-            ft.Row([ft.Text("Sensors num"), ft.TextField(height = 40,width=50)]),
-            ft.Row([ft.Text("Encoding"), ft.TextField(height = 40, width=100)]),
-            ft.Row([ft.Text("Sampling rate"), ft.TextField(height = 40,width=50), ft.Text("Hz")]),
-            ft.Row([ft.Text("max frequency"), ft.TextField(height = 40,width=50), ft.Text("Hz")]),
-            ft.Row([ft.Text("min frequency"), ft.TextField(height = 40,width=50), ft.Text("Hz")]),
-            apply_button
-            ]), padding = 25)
-        log_outputs = ft.Container(content = (ft.Column([
-            ft.Text("Log Outputs"),
-            ft.Container(content = self.log_content, border = ft.border.all(1, "black"), height=400, width=300 )], scroll = ft.ScrollMode.ALWAYS)
-            ),width="", height="" )
+        select_folder_button = ft.Row(
+            [
+                ft.OutlinedButton(text="Select Folder", on_click=self.show_pick_folder),
+                self.target_dir,
+            ]
+        )
+        scan_button = ft.OutlinedButton(text="Scan", on_click=self.on_scan_click)
+        run_button = ft.OutlinedButton(text="Run", on_click=self.on_run_click)
+        open_result_button = ft.OutlinedButton(
+            text="Open Result", on_click=self.on_open_result_click
+        )
+        apply_button = ft.OutlinedButton(text="Apply&Save Settings", on_click="")
+        settings = ft.Container(
+            content=ft.Column(
+                [
+                    ft.Row([ft.Text("Row start"), ft.TextField(height=40, width=50)]),
+                    ft.Row(
+                        [ft.Text("Column start"), ft.TextField(height=40, width=50)]
+                    ),
+                    ft.Row([ft.Text("Sensors num"), ft.TextField(height=40, width=50)]),
+                    ft.Row([ft.Text("Encoding"), ft.TextField(height=40, width=100)]),
+                    ft.Row(
+                        [
+                            ft.Text("Sampling rate"),
+                            ft.TextField(height=40, width=50),
+                            ft.Text("Hz"),
+                        ]
+                    ),
+                    ft.Row(
+                        [
+                            ft.Text("max frequency"),
+                            ft.TextField(height=40, width=50),
+                            ft.Text("Hz"),
+                        ]
+                    ),
+                    ft.Row(
+                        [
+                            ft.Text("min frequency"),
+                            ft.TextField(height=40, width=50),
+                            ft.Text("Hz"),
+                        ]
+                    ),
+                    apply_button,
+                ]
+            ),
+            padding=25,
+        )
+        log_outputs = ft.Container(
+            content=(
+                ft.Column(
+                    [
+                        ft.Text("Log Outputs"),
+                        ft.Container(
+                            content=self.log_content,
+                            border=ft.border.all(1, "black"),
+                            height=400,
+                            width=300,
+                        ),
+                    ],
+                    scroll=ft.ScrollMode.ALWAYS,
+                )
+            ),
+            width="",
+            height="",
+        )
 
-        self.page.add(select_folder_button,
-                ft.Row([ft.Container(content = (ft.Column([
-                settings,
-                scan_button,
-                run_button,
-                open_result_button])
-                ), margin = 10,width = 300),
-                log_outputs])
+        self.page.add(
+            select_folder_button,
+            ft.Row(
+                [
+                    ft.Container(
+                        content=(
+                            ft.Column(
+                                [settings, scan_button, run_button, open_result_button]
+                            )
+                        ),
+                        margin=10,
+                        width=300,
+                    ),
+                    log_outputs,
+                ]
+            ),
         )
         return
 
     def main(self, page: ft.Page):
+
         self.page = page
 
         # page setting
@@ -196,19 +258,21 @@ class MainApp:
         page.controls.append(t)
         self.page.update()
         run_button = ft.OutlinedButton(text="run", on_click=self.on_run_click)
-        read_button = ft.OutlinedButton(text = "read", on_click=self.read_config_file)
-        write_button = ft.OutlinedButton(text = "write", on_click=self.write_config_file)
+        read_button = ft.OutlinedButton(text="read", on_click=self.read_config_file)
+        write_button = ft.OutlinedButton(text="write", on_click=self.write_config_file)
         y = ft.Container(content=ft.Column([run_button, read_button, write_button]))
         page.add(y)
         page.update()
 
 
 class SpectrogramAnalysis:
-    def __init__(self) :
+    def __init__(self):
         self.data = 3
-        #self.frame_range = None
+        # self.frame_range = None
 
-    def run(self,) -> dict[str, Any]:
+    def run(
+        self,
+    ) -> dict[str, Any]:
         self.val = 1 + self.data
         self.answer = {"answer": self.val}
 
@@ -218,14 +282,13 @@ class SpectrogramAnalysis:
         self.max = self.config["SpectrogramAnalysis"]["max"]
         self.min = self.config["SpectrogramAnalysis"]["min"]
 
-
     def export_config(self):
         return self.config["SpectrogramAnalysis"]
 
     def build_result_ui(self):
         self.text_area = ft.Text("設定項目")
         self.val_area = ft.TextField(hint_text="int")
-        x = ft.Container(ft.Row(self.text_area,self.val_area))
+        x = ft.Container(ft.Row(self.text_area, self.val_area))
         return x
 
         # Main_appでのrunでこれも呼ぶ？？
@@ -234,7 +297,6 @@ class SpectrogramAnalysis:
         # 横並びで一塊にして配置したい　https://qiita.com/donraq/items/1ac45ddfe0a803a94e27
         # ここでつくる=>Main_appにUI関係のmake_picみたいなやつを作って並べる
         print("test")
-
 
 
 if __name__ == "__main__":
