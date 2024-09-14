8669d8409cbb5b563b1337aba9875a8fdb095c79
shioneko <shioneko37@gmail.com>
2024-09-14 12:41:28 +0900

    export_yamlを変更項目だけ書き換えに変更

--- utils/yaml_file_handler.py
+++ utils/yaml_file_handler.py
@@ -8,6 +8,7 @@ class YamlFileHandler:
     yamlファイルの読み書きを行う
     Args:
         file_path(str):yamlファイルのパス
+        content(dict[str, Any]): 読み込む項目と初期値　MainAppから渡される
     """
 
     def __init__(self, file_path: str, content: dict[str, Any]):
@@ -16,7 +17,7 @@ class YamlFileHandler:
         if not os.path.isfile(self.file_path):  # yamlファイルが無かったら新規作成
             self.export_yaml()
         self.content = self.import_yaml()
-        # self.content の値は，dictになるようにアプリケーション側で制御
+        self.export_yaml()
 
     def import_yaml(self):
         """
@@ -29,18 +30,27 @@ class YamlFileHandler:
             for key in current_file_content.keys():
                 if key in self.content:
                     self.content[key] = current_file_content[key]
-            print("imported self.content:", self.content)  # 確認用
+        return (self.content)
 
     def export_yaml(self):
         """
         `self.content` をyamlファイルに書き込む
+        'self.content'のうち新しい情報をyamlファイルに書き込む
         """
+        if not os.path.isfile(self.file_path):  # yamlファイルが無かったら新規作成
+            with open(self.file_path, "w") as file:
+                yaml.safe_dump(self.content, file)
+
+        with open(self.file_path, "r") as file:
+            current_file_content: dict[str, Any] = yaml.safe_load(file)
+        for key, value in self.content.items():
+            current_file_content[key] = value
+
         with open(self.file_path, "w") as file:
-            yaml.safe_dump(self.content, file)
-            print("exported self.content:", self.content)  # 確認用
+            yaml.safe_dump(current_file_content, file)
 
 
 if __name__ == "__main__":
-    file_path = "config.yaml"
-    content = {"key3": 2}  # 仮
+    file_path = "configa.yaml"  # 仮
+    content = {"key1": 1, "key3": 2}  # 仮
     YamlFileHandler(file_path, content)
