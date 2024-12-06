9824112c482f07b73a1565c6d1b7f772e8b56ffe
shioneko <shioneko37@gmail.com>
2024-12-06 13:44:56 +0900

    AnalysisResultにFilename付加

--- tremor_analysis/analysis_methods/base.py
+++ tremor_analysis/analysis_methods/base.py
@@ -19,6 +19,8 @@ class AnalysisResult:
     numerical_result: dict[str, Union[int, float]]
     image_result: dict[str, Image.Image]
     analysis_method_class: Type[AnalysisMethodBase]
+    filename1: str
+    filename2: Optional[str]
 
 
 @dataclasses.dataclass
@@ -80,6 +82,8 @@ class AnalysisMethodBase(ABC):
             numerical_result={"value1": 1, "value2": 2},
             image_result={"image1": Image.new("RGB", (1, 1))},
             analysis_method_class=type(self),
+            filename1="file1",
+            filenam2="file2",
         )
 
     def configure_ui(self) -> ft.Control:
