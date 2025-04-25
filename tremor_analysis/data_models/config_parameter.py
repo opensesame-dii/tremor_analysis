0b94665d19e53411308f9ae05cf000065adbb5d9
shioneko <shioneko37@gmail.com>
2025-04-25 15:37:13 +0900

    アクセス方法変更 #34

--- tremor_analysis/data_models/config_parameter.py
+++ tremor_analysis/data_models/config_parameter.py
@@ -4,6 +4,23 @@ from typing import Any, Type, Union
 
 @dataclasses.dataclass
 class ConfigParameter:
-    name: str
+    key: str
+    display_name: str
     value: Any
     type: Union[Type[int], Type[float]]
+
+
+class ConfigList(list[ConfigParameter]):
+    """
+    ConfigParameterのリストを作る
+    """
+
+    def __init__(self, *args: list[ConfigParameter]):
+        super(ConfigList, self).__init__(*args)
+        self.name_to_item = {item.key: item for item in self}
+
+    def __getitem__(self, key: str) -> ConfigParameter:
+        return self.name_to_item[key]
+
+    def __getattr__(self, name: str) -> ConfigParameter:
+        return self.name_to_item[name]
